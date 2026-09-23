fb3bd70e40369d512dcfe44703cb42e77061cda0
CroakingCrow <damienbarragan@gmail.com>
2026-09-24 01:02:32 +0200

    Translated Movedex Texts
    + Pokédex entries

--- data/moves/movedex_text_generics.asm
+++ data/moves/movedex_text_generics.asm
@@ -1,313 +1,307 @@
 _GenericNoEffectText::
-    bage "Aucun effet sup-"
-    next "plémentaire"
+    bage "No additional"
+	next "effect"
     dex
 
 _GenericStopIfUserSwitchText::
-	bage "Cet effet s'annule"
-	next "si le lanceur est"
-	next "rappelé"
+	bage "Ends if the user"
+	next "switches out"
 	dex
 
 _GenericHighCritChancesText::
-	bage "Taux de Critiques"
-	next "élevé"
+	bage "High critical-hit"
+	next "ratio"
 	dex
 
 _Generic2HitsText::
-	bage "Touche 2 fois"
+	bage "Hits twice"
 	dex
 
 _Generic2To5HitsText::
-	bage "Touche 2 à 5 fois"
-	next "à la suite."
-	next "2 coups → 37.5%"
+	bage "Hits 2-5 times in"
+	next "succession."
+	next "2 hits → 37.5%"
 
-	bage "3 coups → 37.5%"
-	next "4 coups → 12.5%"
-	next "5 coups → 12.5%"
+	bage "3 hits → 37.5%"
+	next "4 hits → 12.5%"
+	next "5 hits → 12.5%"
 	dex
 
 _GenericOHKOText::
-	bage "Met l'ennemi KO en"
-	next "1 coup mais a peu"
-	next "de chances de tou-"
+	bage "Always KOs the"
+	next "opponent, but"
+	next "misses a lot."
 
-	bage "cher. Si le lan-"
-	next "ceur est plus lent"
-	next "que l'ennemi,"
-	
-	bage "l'attaque échoue"
+	bage "Doesn't work on"
+	next "opponents with"
+	next "higher Speed"
 	dex
 
 _GenericMissRecoilText::
-	bage "Si la capacité"
-	next "échoue, le lanceur"
-	next "prend 1/8 des"
+	bage "If it fails, the"
+	next "user takes 1/8"
+	next "of the damage"
 	
-	bage "dégâts qu'il au-"
-	next "rait dû infliger"
+	bage "that would have"
+	next "been inflicted"
 	dex
 
 _Generic25PercentRecoilText::
-	bage "L'utilisateur su-"
-	next "bit 25% des dégâts"
-	next "en contrecoup"
+	bage "The user takes 25%"
+	next "of damage done to"
+	next "the foe as recoil"
 	dex
 
 _GenericThrashEffectText::
-	bage "Dure 2 à 3 tours."
-	next "Rend confus l'"
-	next "utilisateur"
+	bage "Lasts 2-3 turns."
+	next "Causes confusion"
+	next "to the user"
 	dex
 
 _GenericTrappingMoveText::
-	bage "Piège pendant 2 à"
-	next "3 tours l'ennemi."
-	next "2 tours → 37.5%"
+	bage "Traps the foe for"
+	next "2-3 turns."
+	next "2 turns → 37.5%"
 
-	bage "3 tours → 37.5%"
+	bage "3 turns → 37.5%"
 	dex
 
 _GenericAbsorbMoveText::
-	bage "Restaure 50% des"
-	next "dégâts infligés"
-	next "à l'utilisateur"
+	bage "Restores 50% of"
+	next "inflicted damage"
+	next "to the user's HP"
 	dex
 
 _GenericHealsHalfText::
-	bage "Soigne l'utilisa-"
-	next "de la moitié de"
-    next "son total de PV"
+	bage "Heals half of"
+	next "the user's max HP"
 	dex
 
 _Generic30PercentBurnText::
-	bage "30% de chance de"
-	next "Brûler l'ennemi"
+	bage "30% chance of"
+	next "burning the foe"
 	dex
 
 _Generic10PercentBurnText::
-	bage "10% de chance de"
-	next "Brûler l'ennemi"
+	bage "10% chance of"
+	next "burning the foe"
 	dex
 
 _Generic10PercentFrozeText::
-	bage "10% de chance de"
-	next "Geler l'ennemi"
+	bage "10% chance of"
+	next "freezing the foe"
+	next "solid"
 	dex
 
 _Generic10PercentParalysisText::
-	bage "10% de chance"
-	next "de Paralyser"
-	next "l'ennemi"
+	bage "10% chance of"
+	next "paralyzing the"
+	next "opponent"
 	dex
 
 _Generic30PercentParalysisText::
-	bage "30% de chance"
-	next "de Paralyser"
-	next "l'ennemi"
+	bage "30% chance of"
+	next "paralyzing the"
+	next "opponent"
 	dex
 
 _GenericParalizeText::
-	bage "Paralyse l'ennemi"
-	next "si la compétence"
-	next "touche"
+	bage "Paralyzes the"
+	next "opponent if the"
+	next "move hits"
 	dex
 
 _Generic20PercentPoisonText::
-	bage "20% de chance"
-	next "d'empoisonner"
-	next "l'ennemi"
+	bage "20% chance of"
+	next "poisoning the"
+	next "foe"
 	dex
 
 _Generic40PercentPoisonText::
-	bage "40% de chance"
-	next "d'empoisonner"
-	next "l'ennemi"
+	bage "40% chance of"
+	next "poisoning the"
+	next "foe"
 	dex
 
 _GenericPoisonText::
-	bage "Empoisonne l'enne-"
-	next "mi si la compé-"
-	next "tence touche"
+	bage "Poisons the"
+	next "opponent if the"
+	next "move hits"
 	dex
 
 _GenericSleepText::
-	bage "Endore l'ennemi"
-	next "si la compétence"
-	next "touche"
+	bage "Puts the foe to"
+	next "sleep if the"
+	next "move hits"
 	dex
 
 _Generic30PercentConfusionText::
-	bage "Rend confus 30%"
-	next "du temps"
+	bage "Causes confusion"
+	next "30% of the time"
 	dex
 
 _Generic10PercentConfusionText::
-	bage "Rend confus 10%"
-	next "du temps"
+	bage "Causes confusion"
+	next "10% of the time"
 	dex
 
 _GenericConfusionText::
-	bage "Rend l'ennemi con-"
-	next "fus si la compé-"
-	next "tence touche"
+	bage "Causes confusion"
+	next "if the move hits"
 	dex
 
 _Generic30PercentFlinchText::
-	bage "Possède 30% de"
-	next "chances d'apeurer"
-	next "l'adversaire"
+	bage "Causes the foe"
+	next "to flinch 30% of"
+	next "the time"
 	dex
 
 _Generic10PercentFlinchText::
-	bage "Possède 10% de"
-	next "chances d'apeurer"
-	next "l'adversaire"
+	bage "Causes the foe"
+	next "to flinch 10% of"
+	next "the time"
 	dex
 
 _GenericAddAtk2Text::
-    bage "Augmente considér-"
-	next "ablement l'Attaque"
-	next "(Atq. +2)"
+    bage "Greatly raises the"
+	next "user's Attack."
+	next "(+2 Atk)"
 	dex
 
 _GenericAddAtk1Text::
-	bage "Augmente l'Attaque"
-	next "du lanceur"
-	next "(Atq. +1)"
+	bage "Raises the user's"
+	next "Attack."
+	next "(+1 Atk)"
 	dex
 
 _GenericLowAtk1Text::
-	bage "Diminue l'Attaque"
-	next "de l'ennemi"
-	next "(Atq. -1)"
+	bage "Lowers the foe's"
+	next "Attack."
+	next "(-1 Atk)"
 	dex
 
 _Generic33PercentLowAtk1Text::
-	bage "33% de chance de"
-	next "diminuer l'Attaque"
-	next "ennemie (Atq. -1)"
+	bage "33% chance to"
+	next "lower the foe's"
+	next "Attack. (-1 Atk)"
 	dex
 
 _GenericAddDef2Text::
-    bage "Augmente considér-"
-	next "ablement la"
-	next "Défense (Déf. +2)"
+    bage "Greatly raises the"
+	next "user's Defense."
+	next "(+2 Def)"
 	dex
 
 _GenericAddDef1Text::
-	bage "Augmente la Déf-"
-	next "ence du lanceur"
-	next "(Déf. +1)"
+	bage "Raises the user's"
+	next "Defense."
+	next "(+1 Def)"
 	dex
 
 _GenericLowDef1Text::
-	bage "Diminue la Défense"
-	next "de l'ennemi"
-	next "(Déf. -1)"
+	bage "Lowers the foe's"
+	next "Defense."
+	next "(-1 Def)"
 	dex
 
 _GenericLowDef2Text::
-    bage "Diminue fortement"
-	next "la Défense ennemie"
-	next "(Déf. -2)"
+    bage "Greatly lowers the"
+	next "foe's Defense."
+	next "(-2 Def)"
 	dex
 
 _Generic33PercentLowDef1Text::
-	bage "33% de chance de"
-	next "diminuer Défense"
-	next "ennemie (Déf. -1)"
+	bage "33% chance to"
+	next "lower the foe's"
+	next "Defense. (-1 Def)"
     dex
 
 _GenericAddSpd2Text::
-    bage "Augmente considér-"
-	next "ablement la"
-	next "Vitesse (Vit. +2)"
+    bage "Greatly raises the"
+	next "user's Speed."
+	next "(+2 Spd)"
 	dex
 
 _Generic33PercentLowSpd1Text::
-	bage "33% de chance de"
-	next "diminuer Vitesse"
-	next "ennemie (Vit. -1)"
+	bage "33% chance to"
+	next "lower the foe's"
+	next "Speed. (-1 Spd)"
 	dex
 
 _GenericLowSpd1Text::
-	bage "Diminue la Vitesse"
-	next "de l'ennemi"
-	next "(Vit. -1)"
+	bage "Lowers the foe's"
+	next "Speed."
+	next "(-1 Spd)"
 	dex
 
 _GenericAddSpe2Text::
-    bage "Augmente considér-"
-	next "ablement le"
-	next "Spécial (Spé. +2)"
+    bage "Greatly raises the"
+	next "user's Special."
+	next "(+2 Spc)"
 	dex
 
 _GenericAddSpe1Text::
-	bage "Augmente le Spé-"
-	next "cial du lanceur"
-	next "(Spé. +1)"
+	bage "Raises the user's"
+	next "Special."
+	next "(+1 Spc)"
 	dex
 
 _Generic33PercentLowSpe1Text::
-	bage "33% de chance de"
-	next "diminuer Spécial"
-	next "ennemi (Spé. -1)"
+	bage "33% chance to"
+	next "lower the foe's"
+	next "Special. (-1 Spc)"
 	dex
 
 _GenericLowPre1Text::
-	bage "Diminue la Préci-"
-	next "sion de l'ennemi."
-	next "(Pré. -1)"
+	bage "Lowers the foe's"
+	next "Accuracy."
+	next "(-1 Acc)"
 	dex
 
 _GenericAddEva1Text::
-	bage "Augmente l'Esquive"
-	next "du lanceur"
-	next "(Esq. +1)"
+	bage "Raises the user's"
+	next "Evasion."
+	next "(+1 Eva)"
 	dex
 
 _GenericAddAtkDef1Text::
-	bage "Augmente l'Attaque"
-	next "et la Défense"
-	next "(Atq et Déf +1)"
+	bage "Raises Attack"
+	next "and Defense."
+	next "(+1 Atk and Def)"
 	dex
 
 _GenericAddAtkPre1Text::
-	bage "Augmente l'Attaque"
-	next "et la Précision"
-	next "(Atq et Pré +1)"
+	bage "Raises Attack"
+	next "and Accuracy."
+	next "(+1 Atk and Acc)"
 	dex
 
 _GenericAddCrit4Text::
-	bage "Augmente les chan-"
-	next "ces de coup Crit-"
-	next "ique par 4"
+	bage "Raises chance of"
+	next "landing critical"
+	next "hits by 4×"
 	dex
 
 _GenericExplodeText::
-	bage "L'utilisateur"
-	next "s'autodétruit,"
-	next "même s'il rate"
+	bage "The user always"
+	next "faints, even if"
+	next "the move misses"
 	dex
 
 _GenericNeverMissText::
-    bage "Ne rate jamais"
+    bage "Never misses"
 	dex
 
 _GenericLevelDamageText::
-	bage "Inflige des dégâts"
-	next "équivalents au"
-	next "niveau du lanceur"
+	bage "Does damage equal"
+	next "to the current"
+	next "level of the user"
 	dex
 
 _GenericImproveCatchRate::
-	bage "Si votre #mon"
-	next "possède cette"
-	next "capacité,"
+	bage "Raises catch-rate"
+	next "ratio if your"
+	next "#mon knows"
 
-	bage "les chances de"
-	next "capturer l'ennemi"
-	next "augmenteront"
+	bage "this move"
 	dex
